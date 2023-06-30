f3b765037efab0c0d22d06a7acec1aab202918d8
RameshAnnabhimoju <ramesh.annabhimoju@gmail.com>
2023-06-30 20:22:46 +0530

    changed to prod url

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import paymentRouter from "./src/routes/payment.router.js";
 const app = express();
-app.use(cors({ origin: process.env.DEV_URL }));
+app.use(cors({ origin: process.env.PROD_URL }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 mongoose
