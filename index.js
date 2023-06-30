68d49a03c50907becdd1206621da0082f1fa1d25
RameshAnnabhimoju <ramesh.annabhimoju@gmail.com>
2023-06-30 18:07:38 +0530

    changed cors to dev

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import mongoose from "mongoose";
 import cors from "cors";
 import paymentRouter from "./src/routes/payment.router.js";
 const app = express();
-app.use(cors({ origin: process.env.DEV_URL, credentials: true }));
+app.use(cors({ origin: process.env.DEV_URL }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  console.log("Connected to DB");
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((error) => console.log(error));
 app.get("/", (request, response) => {
   response.status(200).json({ message: "server working" });
 });
